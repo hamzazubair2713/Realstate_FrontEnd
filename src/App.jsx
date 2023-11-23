eabcb93f16518d4ebf754ad7fc6ac785e9dee0c6
Hamza <hamzazubair1239@gmail.com>
2023-11-23 23:31:46 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Layout from "./components/Layout/Layout";
 
 const App = () => {
-  return <h1 className="text-red-500">App</h1>;
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about-us" element={<About />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
 };
 
 export default App;
